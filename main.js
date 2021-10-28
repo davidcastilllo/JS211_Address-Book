6c6aa4f6213f6d329b3f2f6a52e9723bdaa0b5ef
David Castillo <seth.castillo4@gmail.com>
2021-10-27 22:55:24 -0500

    Finished Project

--- main.js
+++ main.js
@@ -9,16 +9,42 @@ fetch(url)
 
 
  let DisplayBook = (users) => {
-  console.log(users)
   users.forEach(u => {
-   console.log(u.gender)
    let div = document.createElement('div')
    let img = document.createElement('img')
-   img.src = u.picture.medium
+   let name = document.createElement('div')
+   let info = document.createElement('div')
+   let dob = document.createElement('p')
+   let address = document.createElement('p')
+   let button = document.createElement('button')
+   info.appendChild(dob)
+   info.appendChild(address)
    div.appendChild(img)
-   document.body.appendChild(div)
-   div.class = ''
+   div.appendChild(name) 
+   div.appendChild(button)
+   div.append(info)
+   document.getElementById('shell').appendChild(div)
+   div.classList.add('users') 
+   name.classList.add('name')
+   info.id = 'id' + users.indexOf(u)
+   info.classList.add('info')
+   button.classList.add('btn')
+   button.innerHTML = 'Click To Toggle More'
+   // button.id = 'btnId' + users.indexOf(u)
+    button.addEventListener("click", function() {
+     let infoElement = document.getElementById('id'+users.indexOf(u))
+     if (infoElement.style.visibility === 'hidden') {
+      infoElement.style.visibility = 'visible'
+     }
+     else {infoElement.style.visibility = 'hidden'}
+   });
+   name.innerHTML = u.name.first + ' ' + u.name.last
+   img.src = u.picture.medium
+   dob.innerHTML = 'Date of Birth: ' + u.dob.date.slice(0,10) + '  Age: ' + u.dob.age
+   address.innerHTML =  'Address: ' + u.location.street.number + ' ' + u.location.street.name + ', ' + u.location.city
+   info.style.visibility = 'hidden'
   });
  }
+
 
  
